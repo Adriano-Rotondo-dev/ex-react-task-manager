4b034d2cfbc8a76e6fd558ef2dd83693e5f73b09
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2026-01-03 19:02:19 +0100

    fix: update Navbar links to use 'to' prop for routing, add async Task post method

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router";
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
-      <NavLink className="nav-link active p-3" href="#" aria-current="page">
+      <NavLink className="nav-link active p-3" to="/" aria-current="page">
         Lista task
       </NavLink>
-      <NavLink className="nav-link p-3" href="#">
+      <NavLink className="nav-link p-3" to="/add">
         Aggiungi Task
       </NavLink>
     </nav>
